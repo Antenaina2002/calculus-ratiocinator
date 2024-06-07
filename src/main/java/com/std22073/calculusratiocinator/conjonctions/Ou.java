bf496dd479d70ae7ce48dcd53804f20dc8071050
Antenaina2002 <hei.antenaina@gmail.com>
2024-06-07 15:12:13 +0300

    feat: implement logical Ou

---
+++ src/main/java/com/std22073/calculusratiocinator/conjonctions/Ou.java
@@ -0,0 +1,27 @@
+package com.std22073.calculusratiocinator.conjonctions;
+
+import com.std22073.calculusratiocinator.model.Affirmation;
+import com.std22073.calculusratiocinator.model.Conjonction;
+import com.std22073.calculusratiocinator.model.TypeAffirmation;
+
+public class Ou extends Conjonction {
+
+    public Ou(Affirmation affirmation1, Affirmation affirmation2) {
+        super(affirmation1, affirmation2);
+    }
+
+    @Override
+    public boolean estVrai() {
+        return evaluerVerite(getAffirmation1()) && evaluerVerite(getAffirmation2());
+    }
+
+    private boolean evaluerVerite(Affirmation affirmation) {
+        TypeAffirmation type = affirmation.getType();
+        if (type == TypeAffirmation.VERITE) {
+            return true;
+        } else if (type == TypeAffirmation.MENSONGE) {
+            return false;
+        }
+        return false;
+    }
+}
