df2b71d35f7be7c7d851a7030c1e82d14de7e2fd
Antenaina2002 <hei.antenaina@gmail.com>
2024-06-07 13:31:03 +0300

    initial commit

---
+++ src/main/java/com/std22073/calculusratiocinator/CalculusRatiocinatorApplication.java
@@ -0,0 +1,13 @@
+package com.std22073.calculusratiocinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculusRatiocinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculusRatiocinatorApplication.class, args);
+    }
+
+}
