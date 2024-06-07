c25c89cc241c1108a7085d8650d625d99c0be8df
Antenaina2002 <hei.antenaina@gmail.com>
2024-06-07 16:33:05 +0300

    feat: implemented unit test

--- src/test/java/com/std22073/calculusratiocinator/CalculusRatiocinatorApplicationTests.java
+++ src/test/java/com/std22073/calculusratiocinator/CalculusRatiocinatorApplicationTests.java
@@ -1,5 +1,9 @@
 package com.std22073.calculusratiocinator;
 
+import com.std22073.calculusratiocinator.model.Affirmation;
+import com.std22073.calculusratiocinator.conjonctions.Et;
+import com.std22073.calculusratiocinator.conjonctions.Donc;
+import com.std22073.calculusratiocinator.conjonctions.Ou;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +14,20 @@ class CalculusRatiocinatorApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void testCalculerValeurDeVerite() {
+        Affirmation louBeau = new Affirmation("Lou est beau.", Affirmation.TypeAffirmation.VERITE);
+        Affirmation louPauvre = new Affirmation("Lou est pauvre.", Affirmation.TypeAffirmation.MENSONGE);
+        Affirmation louGenereux = new Affirmation("Lou est généreux.", Affirmation.TypeAffirmation.SIMPLE);
+
+        Et louPauvreEtLouGenereux = new Et(louPauvre, louGenereux);
+        Donc louBeauDoncLouPauvre = new Donc(louBeau, louPauvre);
+        Donc louPauvreDoncLouGenereux = new Donc(louPauvre, louGenereux);
+        Ou louBeauOuLouGenereux = new Ou(louBeau, louGenereux);
+
+        System.out.println("Lou est pauvre et Lou est généreux? " + louPauvreEtLouGenereux.estVrai()); // Faux
+        System.out.println("Si Lou est beau alors Lou est pauvre? " + louBeauDoncLouPauvre.estVrai()); // Vrai
+        System.out.println("Si Lou est pauvre alors Lou est généreux? " + louPauvreDoncLouGenereux.estVrai()); // Faux
+        System.out.println("Lou est beau ou Lou est généreux? " + louBeauOuLouGenereux.estVrai()); // Vrai
+    }
 }
