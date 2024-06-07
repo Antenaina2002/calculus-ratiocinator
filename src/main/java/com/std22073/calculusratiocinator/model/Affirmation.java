3df681395e89d94a8a610e81607de3077bb16354
Antenaina2002 <hei.antenaina@gmail.com>
2024-06-07 14:35:57 +0300

    feat: add class for representing logical assertions

---
+++ src/main/java/com/std22073/calculusratiocinator/model/Affirmation.java
@@ -0,0 +1,17 @@
+package com.std22073.calculusratiocinator.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@RequiredArgsConstructor
+@ToString(of = "proposition")
+public class Affirmation {
+
+    private final String proposition;
+    private final TypeAffirmation type;
+    public void afficherValeurDeVerite() {
+        System.out.println("Je ne sais pas comment calculer la valeur de vérité de cette affirmation.");
+    }
+}
